f2af42bda5f35915bb756b43d6378f791f9269cb
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-07-18 00:14:25 +0600

    Create ConfigJsonTest.php

---
+++ Cake/sample-cakephp4/vendor/robmorgan/phinx/tests/Phinx/Config/ConfigJsonTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Test\Phinx\Config;
+
+use Phinx\Config\Config;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+/**
+ * Class ConfigJsonTest
+ * @package Test\Phinx\Config
+ * @group config
+ */
+class ConfigJsonTest extends TestCase
+{
+    /**
+     * @covers \Phinx\Config\Config::fromJson
+     */
+    public function testFromJSONMethod()
+    {
+        $path = __DIR__ . '/_files';
+        $config = Config::fromJson($path . '/valid_config.json');
+        $this->assertEquals('dev', $config->getDefaultEnvironment());
+    }
+
+    /**
+     * @covers \Phinx\Config\Config::fromJson
+     */
+    public function testFromJSONInvalidJson()
+    {
+        $path = __DIR__ . '/_files';
+
+        $this->expectException(RuntimeException::class);
+
+        Config::fromJson($path . '/invalid.json');
+    }
+}
