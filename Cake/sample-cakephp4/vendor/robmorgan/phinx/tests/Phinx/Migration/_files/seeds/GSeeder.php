101ceecec7fcf8cc686adac60b9c814c11216d36
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-07-18 00:20:48 +0600

    Create GSeeder.php

---
+++ Cake/sample-cakephp4/vendor/robmorgan/phinx/tests/Phinx/Migration/_files/seeds/GSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class GSeeder extends AbstractSeed
+{
+    public function run()
+    {
+        $data = [
+            [
+                'body' => 'foo',
+                'created' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'body' => 'bar',
+                'created' => date('Y-m-d H:i:s'),
+            ]
+        ];
+
+        $posts = $this->table('posts');
+        $posts->insert($data)
+              ->save();
+    }
+}
