8d4c733590aa0c6573091f4bdebbc28739f7a442
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-07-18 00:18:10 +0600

    Create IndexTest.php

---
+++ Cake/sample-cakephp4/vendor/robmorgan/phinx/tests/Phinx/Db/Table/IndexTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Test\Phinx\Db\Table;
+
+use Phinx\Db\Table\Index;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+class IndexTest extends TestCase
+{
+    public function testSetOptionThrowsExceptionIfOptionIsNotString()
+    {
+        $column = new Index();
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('"0" is not a valid index option.');
+
+        $column->setOptions(['type']);
+    }
+}
