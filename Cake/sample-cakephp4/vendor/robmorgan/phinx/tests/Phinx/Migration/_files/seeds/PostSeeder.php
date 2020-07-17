e9a6502d5ea5007bfe5f6ae373d5f59491e53cce
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-07-18 00:20:56 +0600

    Create PostSeeder.php

---
+++ Cake/sample-cakephp4/vendor/robmorgan/phinx/tests/Phinx/Migration/_files/seeds/PostSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class PostSeeder extends AbstractSeed
+{
+    public function run()
+    {
+        $data = [
+            [
+                'body' => 'foo',
+                'created' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'body' => 'bar',
+                'created' => date('Y-m-d H:i:s'),
+            ]
+        ];
+
+        $posts = $this->table('posts');
+        $posts->insert($data)
+              ->save();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            'UserSeeder',
+            'GSeeder',
+        ];
+    }
+}
