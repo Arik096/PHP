1321433ccbe1657150092abfea30f3e5f04c3341
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-07-18 00:13:26 +0600

    Create valid_config.php

---
+++ Cake/sample-cakephp4/vendor/robmorgan/phinx/tests/Phinx/Config/_files/valid_config.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    'paths' => [
+        'migrations' => [
+            'application/migrations',
+            'application2/migrations'
+        ],
+        'seeds' => [
+            'application/seeds',
+            'application2/seeds'
+        ]
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'dev',
+        'dev' => [
+            'adapter' => 'mysql',
+            'wrapper' => 'testwrapper',
+            'host' => 'localhost',
+            'name' => 'testing',
+            'user' => 'root',
+            'pass' => '',
+            'port' => 3306
+        ]
+    ]
+];
