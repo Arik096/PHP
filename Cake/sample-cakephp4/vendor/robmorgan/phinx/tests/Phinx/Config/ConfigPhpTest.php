9391ad7d5b8f3580d9803b616cd205aee6d6c273
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-07-18 00:26:45 +0600

    Create ConfigPhpTest.php

---
+++ Cake/sample-cakephp4/vendor/robmorgan/phinx/tests/Phinx/Config/ConfigPhpTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Test\Phinx\Config;
+
+use Phinx\Config\Config;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+/**
+ * Class ConfigPhpTest
+ * @package Test\Phinx\Config
+ * @group config
+ */
+class ConfigPhpTest extends TestCase
+{
+    /**
+     * @covers \Phinx\Config\Config::fromPhp
+     * @covers \Phinx\Config\Config::getDefaultEnvironment
+     */
+    public function testFromPHPMethod()
+    {
+        $path = __DIR__ . '/_files';
+        $config = Config::fromPhp($path . '/valid_config.php');
+        $this->assertEquals('dev', $config->getDefaultEnvironment());
+    }
+
+    /**
+     * @covers \Phinx\Config\Config::fromPhp
+     * @covers \Phinx\Config\Config::getDefaultEnvironment
+     */
+    public function testFromPHPMethodWithoutArray()
+    {
+        $path = __DIR__ . '/_files';
+
+        $this->expectException(RuntimeException::class);
+
+        $config = Config::fromPhp($path . '/config_without_array.php');
+    }
+
+    /**
+     * @covers \Phinx\Config\Config::fromPhp
+     * @covers \Phinx\Config\Config::getDefaultEnvironment
+     */
+    public function testFromJSONMethodWithoutJSON()
+    {
+        $path = __DIR__ . '/_files';
+
+        $this->expectException(RuntimeException::class);
+
+        $config = Config::fromPhp($path . '/empty.json');
+    }
+}
