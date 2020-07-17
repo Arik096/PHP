71ba113f1c877a2f8c82101f9e31358979c78241
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-07-17 23:57:07 +0600

    Update web.php

--- Laravel/My_COVID_Report/routes/web.php
+++ Laravel/My_COVID_Report/routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('ArikCovid','ArikCovidController');
