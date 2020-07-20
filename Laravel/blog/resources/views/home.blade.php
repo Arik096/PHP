145106e0115e12465df34b3e0f8a898c92d147b2
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-07-20 17:49:11 +0600

    blog added

---
+++ Laravel/blog/resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<div class="row">
+    <div class="col-3"><img src="svg/isocial_logo.png" alt="profile pic" style="width:250px;height:100px;"></div>
+    <div class="col-9">
+    <div><h1>iSocial</h1></div>
+
+       <div class="d-flex">
+            <div><strong>100k</strong> posts</div>
+            <div><strong>200k</strong> followers</div>
+            <div><strong>300k</strong> following</div>
+       </div>
+
+       <div class="pt-3 font-weight-bold">arik@isocial.com.bd</div>
+       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam repellat accusantium nemo possimus doloribus atque quidem nulla, amet magnam quae, vero perferendis dicta rem, nam obcaecati similique quis. Voluptatum, eligendi.</div>
+       <div><a href="isocial.com.bd">isocial.com.bd</a></div>
+    </div>
+</div>
+<div class="row">
+<div></div>
+   <div>
+   <img src="svg/isocial_logo.png" alt=""class="w-100">
+  </div>
+  <div>
+   <img src="svg/isocial_logo.png" alt=""class="w-100">
+  </div>
+  <div>
+   <img src="svg/isocial_logo.png" alt=""class="w-100">
+  </div>
+</div>
+</div>
+@endsection
